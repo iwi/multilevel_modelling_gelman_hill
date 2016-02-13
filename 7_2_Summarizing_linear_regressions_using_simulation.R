31103f68bfb715996201e68833ea4f8941bfd83a
iwi <iwi@tuta.io>
2016-02-13 17:12:03 +0000

    Add complete simulation not including parameters uncertainty

--- 7_2_Summarizing_linear_regressions_using_simulation.R
+++ 7_2_Summarizing_linear_regressions_using_simulation.R
@@ -54,21 +54,39 @@ pred.interval <- predict(earn.logmodel.3,
 exp(pred.interval)
 
 # Now obtaining the predicitive interval using simulation
-# The point estimate for log earnings is:
-# 8.4 + 0.017 * 68 - 0.079 * 68 * 1 = 9.95
-# with a sd of 0.88
+
 # or with exp -> 21,000 with sd 2.4
 
-display(earn.logmodel.3)
-earn.logmodel.3$coef[2]
+# The model:
+disp <- display(earn.logmodel.3)
+sd <- disp$sigma.hat
+
+# The point estimate for log earnings is:
+predict_log_earn <- function(height, male) {
+  outcome <-
+    earn.logmodel.3$coef[1] +
+    earn.logmodel.3$coef[2] * height +
+    earn.logmodel.3$coef[3] * male +
+    earn.logmodel.3$coef[4] * height * male
+  return(outcome)
+}
+
+predict_earn(68, 1)
+
+# with a sd of:
+sd  # 0.88
+
+# If we undo the log
+exp(predict_earn(68, 1))
+exp(sd)
 
 # 68% CI [21000/2.4, 21000 * 2.4]
 # 95% CI [21000/2.4^2, 21000 * 2.4^2]
 
 # The prediction
 pred <- exp(rnorm(n = 1000,
-                  mean = 9.95,
-                  sd = 0.88))
+                  mean = predict_log_earn(68, 1),
+                  sd = sd))
 
 # The outcome of the prediction ignoring uncertainty in the regression params
 mean(pred)
@@ -78,13 +96,32 @@ quantile(pred, c(0.025, 0.075))  # 95% interval
 
 # A more complex estimation where simulation is useful
 # Estimating the difference between 68-inch-tall man and woman
+# (just the difference of point estimates for each)
 
-# point estimate = 6900 (just the difference of point estimates for each)
+point_diff <- exp(predict_log_earn(68, 1)) - exp(predict_log_earn(68, 0))
 
+# To get the uncertainty of the combination, simulation helps:
+pred.man <- exp(rnorm(n = 1000,
+                      mean = predict_log_earn(68, 1),
+                      sd = sd))
 
+pred.woman <- exp(rnorm(n = 1000,
+                      mean = predict_log_earn(68, 0),
+                      sd = sd))
 
+pred.diff <- pred.man - pred.woman
 
+pred.ratio <- pred.man / pred.woman
 
-
+# In summary
+hist(pred.man)
+mean(pred.man)
+hist(pred.woman)
+mean(pred.woman)
+hist(pred.diff)
+mean(pred.diff)
+median(pred.diff)
+quantile(pred.diff, c(0.25, 0.75))
+quantile(pred.diff, c(0.025, 0.975))
 
 
