76c5ce83a2f55d8dc2e490799bd82e49f515d9b9
iwi <iwi@tuta.io>
2016-02-14 12:16:05 +0000

    Add 7.3

---
+++ 7_3_Simulation_for_nonlinear_predictions.R
@@ -0,0 +1,11 @@
+# Chapter 7.3 Simulation for nonlinear predictions:
+# congressional elections
+
+# Page 144
+
+## Data are at http://www.stat.columbia.edu/~gelman/arm/examples/...
+
+# dependencies
+library(arm)
+library(foreign)
+library(dplyr)
