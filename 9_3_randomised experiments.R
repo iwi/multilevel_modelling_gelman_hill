532232741ea186069c23bec51d41623c1de656fa
iwi <iwi@tuta.io>
2016-03-17 08:13:26 +0000

    Finalise 9.3 and plot with ggplot2

--- 9_3_randomised experiments.R
+++ 9_3_randomised experiments.R
@@ -27,7 +27,9 @@ resh_electric <- data.frame(
 g <- 1
 n <- length(post.test)
 subset_lm <- function(g) {
-  lin_mod <- lm(post.test ~ treatment,
+  lin_mod <- lm(post.test ~
+                  treatment +
+                  pre.test,
      subset = (graded == g))
 }
 
@@ -41,34 +43,100 @@ lm.2 <- subset_lm(2)
 lm.3 <- subset_lm(3)
 lm.4 <- subset_lm(4)
 
-intercepts <- c(coef(lm.1)[1],
-                coef(lm.2)[1],
-                coef(lm.3)[1],
-                coef(lm.4)[1])
-slopes <- c(coef(lm.1)[2],
-            coef(lm.2)[2],
-            coef(lm.3)[2],
-            coef(lm.4)[2])
+display(lm.1)
 
-coefficients <- data.frame(
-  intercept = intercepts,
-  slope = slopes,
-  grade = as.factor(seq(1:4))
-)
 
-cre <- merge(resh_electric, coefficients)
-View(cre)
 library(dplyr)
 library(ggplot2)
-str(cre)
-cre
+str(resh_electric)
+
 # this plot needs to be fixed...
-ggplot(cre,
+ggplot(resh_electric,
        aes(x = pre.test,
-       y = post.test)) +
+           y = post.test)) +
   geom_point(aes(color = as.factor(treatment))) +
-  geom_abline(intercept = mean(intercept),
-              slope = mean(slope)) +
+  geom_smooth(method = "lm",
+              formula = y ~ x) +
   theme_bw() +
   facet_wrap(~ grade)
 
+resh_electric$gain <- resh_electric$post.test - resh_electric$pre.test
+
+lm.gain <- with(resh_electric,
+                lm(gain ~ treatment))
+
+display(lm.gain)
+
+
+lm.gg <- with(resh_electric,
+                lm(gain ~
+                     treatment +
+                     grade +
+                     treatment * grade))
+
+display(lm.gg)
+
+
+
+
+
+
+# 9.4 Treatment interactions and postratification
+# Interactions of treatment effect with pre-treatment inputs
+lm.4.a <- with(resh_electric,
+               lm(post.test ~ treatment,
+                  subset = (grade == 4)))
+
+display(lm.4.a)
+
+lm.4.b <- with(resh_electric,
+               lm(post.test ~
+                    treatment +
+                    pre.test,
+                  subset = (grade == 4)))
+
+display(lm.4.b)
+
+lm.4.c <- with(resh_electric,
+               lm(post.test ~
+                    treatment +
+                    pre.test +
+                    treatment * pre.test,
+                  subset = (grade == 4)))
+
+display(lm.4.c)
+
+lm.4.c.sim <- sim(lm.4.c, 1000)
+str(lm.4.c.sim)
+
+# Filter out those that are not in grade 4
+resh_electric %>%
+  filter(grade == 4) ->
+  re4
+
+# Take a look at the domain (80 - 125)
+hist(re4$pre.test)
+
+# Calculate the treatment effect with the linear model
+re4$treatment_effect <-
+  coef(lm.4.c)[2] +
+  coef(lm.4.c)[4] * re4$pre.test
+
+# Plot the pre-test outcome vs. the treatment effect and show (in gray)
+# the uncertainty brought by the coefficients
+p <- ggplot(re4,
+            aes(x = pre.test,
+                y = treatment_effect)) +
+  geom_point() +
+  theme_bw()
+
+for (i in 1:500) {
+  p <- p +  geom_abline(intercept = coef(lm.4.c.sim)[i, 2],
+                          slope = coef(lm.4.c.sim)[i, 4],
+                          colour = 'grey')
+}
+p
+p <- p +  geom_abline(intercept = coef(lm.4.c)[2],
+                        slope = coef(lm.4.c)[4],
+                        colour = 'red')
+p
